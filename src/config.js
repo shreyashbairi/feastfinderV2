3acf717061f95f7320cab5e4d584bdc24d28055d
shreyashbairi <sbairi@purdue.edu>
2024-11-15 18:54:07 -0500

    temp change

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const GAPIKEY = process.env.REACT_APP_GAPI_KEY;
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+//export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+export const API_BASE_URL = 'https://feastfinder-vn6l.onrender.com';
+console.log('API_BASE_URL:', API_BASE_URL);
